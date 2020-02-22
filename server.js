b7a5793023a8d7650232245ac11d78a0729e0554
festustp <festustp@gmail.com>
2020-02-22 10:16:17 +0100

    changing pg

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : 'Handsomegideon1.',
+    password : 'fayokunmi',
     database : 'brain'
   }
 })
